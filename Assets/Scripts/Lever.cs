ba1a03e924bf356bd565caabee8da48e162088cb
AgusSoto03 <agustinsoto2003@gmail.com>
2025-09-28 18:04:27 -0300

    Update de las plataformas
    Agregue que las plataformas cuando las activas en el mundo de Alba se muevan en el de ocaso cuando se activa el mundo

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -1,58 +1,42 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
-
-public class Lever : MonoBehaviour
+public class Lever : MonoBehaviour 
 {
-    [Header("Configuraci�n")]
-    public float interactionRange = 3f;
-    public KeyCode interactKey = KeyCode.F;
+    [Header("Configuración")] 
+    public float interactionRange = 3f; 
+    public KeyCode interactKey = KeyCode.F; 
     public GameObject uiIndicator; 
-    public UnityEvent onLeverActivated;
-
-    private GameObject player;
-    private bool isActivated = false;
-
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-        if (uiIndicator != null)
-            uiIndicator.SetActive(false);
-    }
-
-    void Update()
-    {
-        if (player == null) return;
-
-        float distance = Vector2.Distance(player.transform.position, transform.position);
-
-
-        if (distance <= interactionRange && !isActivated)
-        {
-            if (uiIndicator != null)
-                uiIndicator.SetActive(true);
-
-            if (Input.GetKeyDown(interactKey))
-            {
-                ToggleLever();
-            }
-        }
-        else
-        {
-            if (uiIndicator != null)
-                uiIndicator.SetActive(false);
-        }
-    }
-
-    void ToggleLever()
-    {
-        if (isActivated) return;
-
-        isActivated = true;
-
-        Debug.Log("Palanca activada!");
-        onLeverActivated?.Invoke();
-
-        if (uiIndicator != null)
-            uiIndicator.SetActive(false);
-    }
+    public UnityEvent onLeverActivated; 
+    private GameObject player; 
+    private bool isActivated = false; 
+    void Start() 
+    { 
+        player = GameObject.FindGameObjectWithTag("Player"); 
+        if (uiIndicator != null) uiIndicator.SetActive(false); 
+    } 
+    void Update() 
+    { 
+        if (player == null) return; 
+        float distance = Vector2.Distance(player.transform.position, transform.position); 
+        if (distance <= interactionRange && !isActivated) 
+        { 
+            if (uiIndicator != null) uiIndicator.SetActive(true); 
+            if (Input.GetKeyDown(interactKey)) 
+            { 
+                ToggleLever(); 
+            } 
+        } 
+        else 
+        { 
+            if (uiIndicator != null) uiIndicator.SetActive(false); 
+        } 
+    } 
+    void ToggleLever() 
+    { 
+        if (isActivated) return; 
+        isActivated = true; 
+        Debug.Log("Palanca activada!"); 
+        onLeverActivated?.Invoke(); 
+        if (uiIndicator != null) uiIndicator.SetActive(false); 
+    } 
 }
