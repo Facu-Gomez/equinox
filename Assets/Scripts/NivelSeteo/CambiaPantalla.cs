10d28650b09726ec0766b06ebda79f1b5f348efc
Geno <santi-condori-huanca@hotmail.com>
2025-10-29 15:28:07 -0300

    Pequeño arreglo

--- Assets/Scripts/NivelSeteo/CambiaPantalla.cs
+++ Assets/Scripts/NivelSeteo/CambiaPantalla.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class CambiaPantalla : MonoBehaviour
 {
-    [SerializeField] Camera camara;
+  
     [SerializeField] GameObject pantallaCarga;
     [SerializeField] GameObject MundoOcaso;
     [SerializeField] GameObject MundoAlba;
@@ -24,17 +24,10 @@ public class CambiaPantalla : MonoBehaviour
         MundoAlba.SetActive(false);
         MundoOcaso.SetActive(false);
         pantallaCarga.SetActive(true);
-        SetMainCamera(camara);
+    
         Debug.Log("El jugador ha caido en la zona de caida.");
         // Aquí puedes agregar la lógica que deseas ejecutar cuando el jugador cae en la zona de caída.
-void SetMainCamera(Camera nuevaCamara)
-{
-    if (Camera.main != null)
-        Camera.main.gameObject.SetActive(false);
 
-    nuevaCamara.gameObject.SetActive(true);
-    nuevaCamara.tag = "MainCamera";
-}
 
     }
     
